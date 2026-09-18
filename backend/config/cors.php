3420efba47ee745d205a06aefb617ac803f2da23
himansh-hu <himanshujdubey9@gmail.com>
2026-09-18 10:33:16 +0530

    fix api cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,12 +1,42 @@
+
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing.
+    | These settings determine which cross-origin operations may execute.
+    |
+    */
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => [
+        '*',
+    ],
+
+    'allowed_origins' => [
+        'https://vyojin-av26.vercel.app',
+    ],
+
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+
+    'allowed_headers' => [
+        '*',
+    ],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
+
 ];
+
